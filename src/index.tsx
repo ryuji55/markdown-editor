dec59229a49e2aa727d67a459a1feb0166ee6215
ryuji55 <rt.october.2@icloud.com>
2022-04-05 22:02:26 +0900

    Add:エディタに入力可能にした

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { render } from 'react-dom'
+import * as React from 'react';
+import { render } from 'react-dom';
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
